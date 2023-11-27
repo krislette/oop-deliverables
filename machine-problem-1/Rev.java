73614537e8917eb7431ea2971306067e8b9d3014
Krislette <acellekrislette@gmail.com>
2023-11-27 23:52:45 +0800

    added error handling

--- machine-problem-1/Rev.java
+++ machine-problem-1/Rev.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Rev {
@@ -7,39 +8,56 @@ public class Rev {
     public static final int INT_MAX_EMPLOYEES = 5;
     public static final int INT_HOURLY_RATE = 250;
     public static final int INT_WORK_HOURS = 8;
+    public static final int INT_DASH_LENGTH = 65;
+    public static final String STR_DASH = "-";
 
     public static void main(String[] args) {
-        int[] arrDailyWorkedHours = new int[INT_DAYS_PER_WEEK];
-        int[] arrDailySalaries = new int[INT_DAYS_PER_WEEK];
-        int[] arrUnderTime = new int[INT_DAYS_PER_WEEK];
-        int[] arrOverTime = new int[INT_DAYS_PER_WEEK];
+        displayDashes();
+        System.out.println("\t\tEmployee Weekly Time Record (DTR)");
+        displayDashes();
 
-        int intEmployeeCount = 0;
-        while (intEmployeeCount < INT_MAX_EMPLOYEES) {
+        for (int intEmployeeCount = 0; intEmployeeCount < INT_MAX_EMPLOYEES; intEmployeeCount++) {
+            System.out.println("\t\t\t   EMPLOYEE " + (intEmployeeCount + 1));
+            displayDashes();
+
+            System.out.print("Enter employee ID: ");
+            String strEmployeeID = input.nextLine();
+
             System.out.print("Enter employee name: ");
             String strEmployeeName = input.nextLine();
-    
+
+            int[] arrDailyWorkedHours = new int[INT_DAYS_PER_WEEK];
+            int[] arrDailySalaries = new int[INT_DAYS_PER_WEEK];
+            int[] arrUnderTime = new int[INT_DAYS_PER_WEEK];
+            int[] arrOverTime = new int[INT_DAYS_PER_WEEK];
+            int intWeeklyTotalIncome = 0;
+
             for (int i = 0; i < INT_DAYS_PER_WEEK; i++) {
-                System.out.print("Enter hours worked on day " + (i + 1) + ": ");
-                arrDailyWorkedHours[i] = input.nextInt();
+                // System.out.print("Enter hours worked on day " + (i + 1) + ": ");
+                arrDailyWorkedHours[i] = getInput(i);
                 arrDailySalaries[i] = calculateDailySalary(arrDailyWorkedHours[i]);
                 arrUnderTime[i] = calculateUnderTime(arrDailyWorkedHours[i]);
                 arrOverTime[i] = calculateOverTime(arrDailyWorkedHours[i]);
+                intWeeklyTotalIncome = calculateWeeklyTotalIncome(intWeeklyTotalIncome, arrDailySalaries[i]);
             }
-    
-            System.out.println("Employee Weekly Daily Time Record for " + strEmployeeName);
-    
+
+            System.out.println("\nEmployee ID of " + strEmployeeName + ": " + strEmployeeID);
+            System.out.println("Employee Weekly Daily Time Record for " + strEmployeeName + ":");
+
             for (int i = 0; i < INT_DAYS_PER_WEEK; i++) {
-                System.out.println("Day " + (i + 1) + ":");
+                System.out.println("\nDay " + (i + 1) + ":");
                 System.out.println("Hours Worked: " + arrDailyWorkedHours[i]);
-                System.out.println("Daily Salary: PHP" + arrDailySalaries[i]);
+                System.out.println("Daily Salary: PHP " + arrDailySalaries[i]);
                 System.out.println("Under Time: " + arrUnderTime[i] + " hour(s)");
                 System.out.println("Over Time: " + arrOverTime[i] + " hour(s)");
             }
 
-            intEmployeeCount++;
+            System.out.println("\nWeekly Total Income: PHP " + intWeeklyTotalIncome);
             input.nextLine();
+            displayDashes();
         }
+
+        System.out.println("Thank you for using Employee Weekly Time Record.");
 
         input.close();
         System.exit(0);
@@ -55,6 +73,29 @@ public class Rev {
 
     public static int calculateOverTime(int intHoursWorked) {
         return ((intHoursWorked - INT_WORK_HOURS) > 0) ? intHoursWorked - INT_WORK_HOURS : 0;
+    }
+
+    public static int calculateWeeklyTotalIncome(int intWeeklyTotalIncome, int intDailySalary) {
+        return intWeeklyTotalIncome += intDailySalary;
+    }
+
+    public static void displayDashes() {
+        System.out.println(STR_DASH.repeat(INT_DASH_LENGTH));
+    }
+
+    public static int getInput(int intIterationCount) {
+        int intIntegerInput;
+
+        try {
+            System.out.print("Enter hours worked on day " + (intIterationCount + 1) + ": ");
+            intIntegerInput = input.nextInt();
+        } catch (InputMismatchException e) {
+            System.out.println("Invalid input. Enter integer values only.");
+            input.nextLine();
+            return getInput(intIterationCount);
+        }
+
+        return intIntegerInput; 
     }
 
 }
